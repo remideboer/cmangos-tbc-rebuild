fe584a429c9592766fe8e2316ce19db68761253f
Remi de Boer <remideboer@yahoo.com>
2026-09-10 13:43:30 +0200

    feat(inventory): send DEFENSE_SKILL_RATING on CMSG_AUTOSTORE_BAG_ITEM (TP-SL14-013)

--- tbc-world/src/main/java/org/tbc/world/session/InventoryHandler.java
+++ tbc-world/src/main/java/org/tbc/world/session/InventoryHandler.java
@@ -393,7 +393,8 @@ public final class InventoryHandler {
                         UpdateFields.UNIT_FIELD_RESISTANCES + 5,
                         UpdateFields.UNIT_FIELD_RESISTANCES + 6,
                         UpdateFields.PLAYER_FIELD_COMBAT_RATING_1 + 5,
-                        UpdateFields.PLAYER_FIELD_COMBAT_RATING_1 + 6));
+                        UpdateFields.PLAYER_FIELD_COMBAT_RATING_1 + 6,
+                        UpdateFields.PLAYER_FIELD_COMBAT_RATING_1 + 1));
         s.send(pkt.opcode(), pkt.payload());
     }
 
